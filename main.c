7d26f07eef50f3f55ae7319806dcd6d72fbe8ef8
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-11-24 16:21:52 +0200

    Add `getline`-`printf` loop

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "lib/object.c"
 
 #include <stdlib.h>
+#include <stdio.h>
+
+static const char prompt[] = "vesihiisi> ";
 
 int main(int /*argc*/, char** /*argv*/) {
+    for (;/*ever*/;) {
+        printf("%s", prompt);
+        
+        char* line = NULL;
+        size_t len = 0;
+        if (getline(&line, &len, stdin) != -1) {
+            printf("%s", line);
+        } else {
+            puts("Error reading input");
+            
+            return EXIT_FAILURE;
+        }
+
+        free(line);
+    }
+
     return EXIT_SUCCESS;
 }
 

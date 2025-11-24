3fea285873f7044754d917a2b64580e82df3ee98
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-11-24 15:45:43 +0200

    Add object header

--- lib/object.c
+++ lib/object.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 
-typedef struct {
+typedef struct ORef {
     uint32_t bits;
 } ORef;
 
@@ -19,4 +19,18 @@ inline static bool isFixnum(ORef oref) { return !(oref.bits & tag_bits); }
 inline static bool isFlonum(ORef oref) { return (oref.bits & tag_bits) == flonum_tag; }
 
 inline static bool isMiscScalar(ORef oref) { return (oref.bits & tag_bits) == miscScalar_tag; }
+
+typedef void* HeapRef;
+
+typedef struct Header {
+    uint32_t bits;
+} Header;
+
+static const uint32_t header_tag_bits = 0b11;
+
+static const uint32_t slots_tag = 0b1;
+
+static const uint32_t bytes_tag = 0b10;
+
+inline static Header header(HeapRef href) { return *((Header*)href - 1); }
 

04e72de362ba387121ccc73d6f8c496cefc90e79
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-12-16 13:29:00 +0200

    `eq?` -> `identical?`

--- test/facts.lisp
+++ test/facts.lisp
@@ -1,19 +1,19 @@
 (def grec-fact
   (fn (n)
-    (if (eq? n 0)
+    (if (identical? n 0)
       1
       (fx* n (grec-fact (fx- n 1))))))
 
 (def lrec-fact
   (letfn ((fact (n)
-            (if (eq? n 0)
+            (if (identical? n 0)
               1
               (fx* n (fact (fx- n 1))))))
     fact))
 
 (def trec-fact
   (letfn ((fact (acc n)
-            (if (eq? n 0)
+            (if (identical? n 0)
               acc
               (fact (fx* acc n) (fx- n 1)))))
     (fn (n) (fact 1 n))))

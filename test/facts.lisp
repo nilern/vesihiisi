45eaabc2e2c5b3fb80a7e8cd4d53d67a48bfa02d
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2025-12-27 20:41:46 +0200

    Update tentative `letfn` uses

--- test/facts.lisp
+++ test/facts.lisp
@@ -5,14 +5,14 @@
       (fx* n (grec-fact (fx- n 1))))))
 
 (def lrec-fact
-  (letfn ((fact (n)
+  (letfn (((fact n)
             (if (identical? n 0)
               1
               (fx* n (fact (fx- n 1))))))
     fact))
 
 (def trec-fact
-  (letfn ((fact (acc n)
+  (letfn (((fact acc n)
             (if (identical? n 0)
               acc
               (fact (fx* acc n) (fx- n 1)))))

a6c27b83219ad4fd54cbc6a7d1ffa17f640bdb75
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-01-04 15:13:38 +0200

    Make dispatch header more representative

--- lib/dispatch.h
+++ lib/dispatch.h
@@ -3,10 +3,9 @@
 #include "state.h"
 
 // OPTIMIZE: We know the domain of primops at VM compile time so going through this is suboptimal:
+/// Returns (first) error, `Zero` if none:
 [[nodiscard]]
 static ORef checkDomain(State* state);
 
-[[nodiscard]]
-static bool closureIsApplicable(State const* state, Closure const* callee);
-
+/// Returns applicable closure from `callee`, `Zero` if none is found:
 static ORef applicableClosure(State const* state, Multimethod const* callee);

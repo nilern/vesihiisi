e3a0ad7d0da6cdc79d14ca9c8c107d0f0c746467
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-01-12 20:54:59 +0200

    Add cpstak benchmark

--- bench/scheme/bench.sh
+++ bench/scheme/bench.sh
@@ -2,3 +2,6 @@
 
 cat ./prelude.lisp ../../base/bootstrap.lisp ./gabriel/tak.lisp ./postlude.lisp \
     | ../../vesihiisi -
+
+cat ./prelude.lisp ../../base/bootstrap.lisp ./gabriel/cpstak.lisp ./postlude.lisp \
+    | ../../vesihiisi -
---
+++ bench/scheme/gabriel/cpstak.lisp
@@ -0,0 +1,33 @@
+;;; CPSTAK -- A continuation-passing version of the TAK benchmark.
+;;; A good test of first class procedures and tail recursion.
+
+(def cpstak
+  (fn (x y z)
+    (letfn (((tak x y z k)
+              (if (not (< y x))
+                (k z)
+                (tak (- x 1)
+                     y
+                     z
+                     (fn (v1)
+                       (tak (- y 1)
+                            z
+                            x
+                            (fn (v2)
+                              (tak (- z 1)
+                                   x
+                                   y
+                                   (fn (v3)
+                                     (tak v1 v2 v3 k))))))))))
+      (tak x y z (fn (a) a)))))
+
+(def main
+  (fn args
+    (run-benchmark
+      "cpstak"
+      1000
+      (fn (result) (= result 7))
+      (fn (x y z) (fn () (cpstak x y z)))
+      18
+      12
+      6)))

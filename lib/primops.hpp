317c8b70bc4c75fbd5155d8208d43a1aa4a7fc54
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-01-17 18:07:58 +0200

    Move primop fwd decls to the header where they belong

--- lib/primops.hpp
+++ lib/primops.hpp
@@ -8,5 +8,40 @@ namespace {
 ORef getErrorHandler(State const* state);
 
 PrimopRes callBytecode(State* state);
+PrimopRes primopAbort(State* state);
+PrimopRes primopApplyArray(State* state);
+PrimopRes primopApplyList(State* state);
+PrimopRes primopCallCC(State* state);
+PrimopRes primopContinue(State* state);
+PrimopRes primopIdentical(State* state);
+PrimopRes primopTypeOf(State* state);
+PrimopRes primopMakeSlotsType(State* state);
+PrimopRes primopMake(State* state);
+PrimopRes primopSlotGet(State* state);
+PrimopRes primopSlotSet(State* state);
+PrimopRes primopMakeFlex(State* state);
+PrimopRes primopFlexCount(State* state);
+PrimopRes primopFlexGet(State* state);
+PrimopRes primopFlexCopy(State* state);
+PrimopRes primopFxAdd(State* state);
+PrimopRes primopFxSub(State* state);
+PrimopRes primopFxMul(State* state);
+PrimopRes primopFxQuot(State* state);
+PrimopRes primopFxLt(State* state);
+PrimopRes primopFixnumToFlonum(State* state);
+PrimopRes primopFlAdd(State* state);
+PrimopRes primopFlSub(State* state);
+PrimopRes primopFlMul(State* state);
+PrimopRes primopFlDiv(State* state);
+PrimopRes primopCharIsAlphabetic(State* state);
+PrimopRes primopCharIsWhitespace(State* state);
+PrimopRes primopStringIteratorNext(State* state);
+PrimopRes primopWrite(State* state);
+PrimopRes primopWriteChar(State* state);
+PrimopRes primopWriteString(State* state);
+PrimopRes primopCurrentSecond(State* state);
+PrimopRes primopCurrentJiffy(State* state);
+PrimopRes primopJiffiesPerSecond(State* state);
+PrimopRes primopExit(State* state);
 
 } // namespace
--- lib/state.cpp
+++ lib/state.cpp
@@ -9,6 +9,7 @@
 #include "object.hpp"
 #include "heap.hpp"
 #include "flyweights.hpp"
+#include "primops.hpp"
 
 namespace {
 
@@ -636,43 +637,6 @@ inline Type* tryCreateCharType(Semispace* semispace, Type const* typeType) {
 inline Type* tryCreateBoolType(Semispace* semispace, Type const* typeType) {
     return tryCreateImmType(semispace, typeType);
 }
-
-PrimopRes callBytecode(State* state);
-PrimopRes primopAbort(State* state);
-PrimopRes primopApplyArray(State* state);
-PrimopRes primopApplyList(State* state);
-PrimopRes primopCallCC(State* state);
-PrimopRes primopContinue(State* state);
-PrimopRes primopIdentical(State* state);
-PrimopRes primopTypeOf(State* state);
-PrimopRes primopMakeSlotsType(State* state);
-PrimopRes primopMake(State* state);
-PrimopRes primopSlotGet(State* state);
-PrimopRes primopSlotSet(State* state);
-PrimopRes primopMakeFlex(State* state);
-PrimopRes primopFlexCount(State* state);
-PrimopRes primopFlexGet(State* state);
-PrimopRes primopFlexCopy(State* state);
-PrimopRes primopFxAdd(State* state);
-PrimopRes primopFxSub(State* state);
-PrimopRes primopFxMul(State* state);
-PrimopRes primopFxQuot(State* state);
-PrimopRes primopFxLt(State* state);
-PrimopRes primopFixnumToFlonum(State* state);
-PrimopRes primopFlAdd(State* state);
-PrimopRes primopFlSub(State* state);
-PrimopRes primopFlMul(State* state);
-PrimopRes primopFlDiv(State* state);
-PrimopRes primopCharIsAlphabetic(State* state);
-PrimopRes primopCharIsWhitespace(State* state);
-PrimopRes primopStringIteratorNext(State* state);
-PrimopRes primopWrite(State* state);
-PrimopRes primopWriteChar(State* state);
-PrimopRes primopWriteString(State* state);
-PrimopRes primopCurrentSecond(State* state);
-PrimopRes primopCurrentJiffy(State* state);
-PrimopRes primopJiffiesPerSecond(State* state);
-PrimopRes primopExit(State* state);
 
 HRef<Method> vcreatePrimopMethod(
     State* state, Str name, MethodCode nativeCode, bool hasVarArg, Fixnum arity, va_list domain);

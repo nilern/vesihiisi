a6a58ce0e935a2b6e9227414c1906817ed47f19c
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-01-19 11:21:46 +0200

    Comment primops header

--- lib/primops.hpp
+++ lib/primops.hpp
@@ -7,14 +7,24 @@ namespace {
 
 ORef getErrorHandler(State const* state);
 
+// Primops
+// =================================================================================================
+
+// Pseudo-Operation
 PrimopRes callBytecode(State* state);
+
+// Control Flow
 PrimopRes primopAbort(State* state);
 PrimopRes primopApplyArray(State* state);
 PrimopRes primopApplyList(State* state);
 PrimopRes primopCallCC(State* state);
 PrimopRes primopContinue(State* state);
+
+// For Any Value
 PrimopRes primopIdentical(State* state);
 PrimopRes primopTypeOf(State* state);
+
+// Heap Objects
 PrimopRes primopMakeSlotsType(State* state);
 PrimopRes primopMake(State* state);
 PrimopRes primopSlotGet(State* state);
@@ -25,29 +35,43 @@ PrimopRes primopFlexGet(State* state);
 PrimopRes primopFlexSet(State* state);
 PrimopRes primopFlexCopy(State* state);
 PrimopRes primopFlexClone(State* state);
+
+// Fixnums
 PrimopRes primopFxAdd(State* state);
 PrimopRes primopFxSub(State* state);
 PrimopRes primopFxMul(State* state);
 PrimopRes primopFxQuot(State* state);
 PrimopRes primopFxLt(State* state);
 PrimopRes primopFixnumToFlonum(State* state);
+
+// Flonums
 PrimopRes primopFlAdd(State* state);
 PrimopRes primopFlSub(State* state);
 PrimopRes primopFlMul(State* state);
 PrimopRes primopFlDiv(State* state);
+
+// Characters
 PrimopRes primopCharLt(State* state);
 PrimopRes primopCharToInteger(State* state);
 PrimopRes primopCharIsAlphabetic(State* state);
 PrimopRes primopCharIsWhitespace(State* state);
+
+// Strings
 PrimopRes primopArrayMutToString(State* state);
 PrimopRes primopStringIteratorNext(State* state);
 PrimopRes primopStringToSymbol(State* state);
+
+// I/O
 PrimopRes primopWrite(State* state);
 PrimopRes primopWriteChar(State* state);
 PrimopRes primopWriteString(State* state);
+
+// Time
 PrimopRes primopCurrentSecond(State* state);
 PrimopRes primopCurrentJiffy(State* state);
 PrimopRes primopJiffiesPerSecond(State* state);
+
+// System Interface
 PrimopRes primopExit(State* state);
 
 } // namespace

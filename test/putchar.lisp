c759c97804f3fbccc4c5b556e935adb1b33d0631
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-02-11 16:33:28 +0200

    Add anticipatory FFI test

---
+++ test/putchar.lisp
@@ -0,0 +1,14 @@
+(define libc (open-foreign-library "libc.so.6"))
+
+(define putchar
+  (let ((putchar (get-foreign libc "putchar")))
+    (fn ((: c <char>))
+      (call-foreign <fixnum> putchar c))))
+
+(define main
+  (fn ()
+    (let ((res (putchar #"c")))
+      (close-foreign-library libc)
+      res)))
+
+(main)

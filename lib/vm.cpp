b383cd3640fd363befd275f91380056c41da41f0
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-02-12 20:46:29 +0200

    Fix `!=` -> `>`

--- lib/vm.cpp
+++ lib/vm.cpp
@@ -378,8 +378,8 @@ VMRes run(RT* rt, HRef<Closure> self) {
             auto const codomain = HRef<Type>::fromUnchecked(anyCodomain);
             bool const fRet = eq(codomain, rt->types.flonum);
             if (codomain->isFlex.val()) { PANIC("Flex FFI return type"); } // TODO: Proper error
-            if (codomain->minSize.val() != sizeof(uint64_t)) {
-                PANIC("FFI return type size %ld != word size %lu\n",
+            if (size_t(codomain->minSize.val()) > sizeof(uint64_t)) {
+                PANIC("FFI return type size %ld > word size %lu\n",
                       codomain->minSize.val(), sizeof(uint64_t));
             }
             ORef const* fPtr = codomainPtr + 1;

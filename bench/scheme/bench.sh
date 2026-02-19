90e470621b3756321b311798fb1dc45c21b2a5f8
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-02-19 13:02:40 +0200

    Finally run the benchmarks with a simple shell loop

--- bench/scheme/bench.sh
+++ bench/scheme/bench.sh
@@ -2,26 +2,7 @@
 
 export VSHS_HOME=../..
 
-cat ./prelude.lisp ./gabriel/browse.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/cpstak.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/ctak.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/deriv.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/destruc.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/diviter.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/divrec.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/nboyer.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/puzzle.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/tak.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/takl.lisp ./postlude.lisp | ../../vesihiisi -
-
-cat ./prelude.lisp ./gabriel/triangl.lisp ./postlude.lisp | ../../vesihiisi -
+for benchmark in gabriel/*.lisp
+do
+  cat ./prelude.lisp $benchmark ./postlude.lisp | ../../vesihiisi -
+done

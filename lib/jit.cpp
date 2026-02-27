d910ee1ff23a4e394904c5dc28f45151823ac0d8
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-02-27 12:29:43 +0200

    Use `Assembler` (as `Compiler` has no tail call support)

--- lib/jit.cpp
+++ lib/jit.cpp
@@ -7,16 +7,10 @@ namespace {
 void jitCompile(RT& rt, Method& method) {
     asmjit::CodeHolder code;
     code.init(rt.jit.environment(), rt.jit.cpu_features());
-    auto cc = asmjit::x86::Compiler(&code);
+    auto as = asmjit::x86::Assembler(&code);
 
-    cc.add_func(asmjit::FuncSignature::build<PrimopRes, RT*>());
-
-    asmjit::x86::Gp const virt_reg = cc.new_gp64();
-    cc.mov(virt_reg, PrimopRes::CALL_BYTECODE);
-    cc.ret(virt_reg);
-
-    cc.end_func();
-    cc.finalize();
+    as.mov(asmjit::x86::rax, PrimopRes::CALL_BYTECODE);
+    as.ret();
 
     MethodCode* entryCode = reinterpret_cast<MethodCode*>(method.code->itemsMut().data());
     if (rt.jit.add(entryCode, &code) != asmjit::Error::kOk) { PANIC("JIT miscompilation"); }

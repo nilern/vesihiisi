946f44c3ce0e5f658ba6d018d8ebee86d15dafa8
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2026-03-02 11:30:29 +0200

    Add `X64SYSVJIT::naturalize` scaffolding

--- lib/jit.cpp
+++ lib/jit.cpp
@@ -13,6 +13,8 @@ class X64SYSVJIT {
     asmjit::CodeHolder code_;
     asmjit::x86::Assembler as_;
 
+    void naturalize(std::span<uint8_t const> bytecode);
+
 public:
     X64SYSVJIT(RT& rt) :
         rt_{&rt}, code_{},
@@ -25,106 +27,138 @@ public:
     static constexpr asmjit::x86::Gp rtReg = asmjit::x86::rdi;
     static constexpr asmjit::x86::Gp retReg = asmjit::x86::rax;
 
-    void jitMethod(Method& method) {
-        using namespace asmjit;
+    void jitMethod(Method& method);
+};
 
-        if (!std::ranges::all_of(method.domain(),
-                                 [&](ORef type) { return eq(type, rt_->types.any); })
+void X64SYSVJIT::naturalize(std::span<uint8_t const> bytecode) {
+    auto const end = bytecode.end();
+    for (auto it = bytecode.begin(); it != end;) {
+        // TODO: Actually JIT-compile the bytecodes:
+        switch (static_cast<Opcode>(*it++)) {
+        case OP_MOVE:
+        case OP_SWAP:
+        case OP_DEFINE:
+        case OP_GLOBAL_SET:
+        case OP_GLOBAL:
+        case OP_CONST:
+        case OP_SPECIALIZE:
+        case OP_KNOT:
+        case OP_KNOT_INIT:
+        case OP_KNOT_GET:
+        case OP_BRF:
+        case OP_BR:
+        case OP_RET:
+        case OP_CLOSURE:
+        case OP_CLOVER:
+        case OP_CALL:
+        case OP_TAILCALL:
+        case OP_FFICALL:  {
+            as_.mov(X64SYSVJIT::retReg, PrimopRes::INTERPRET);
+            as_.ret();
+            return;
+        }; break;
+        }
+    }
+}
+
+void X64SYSVJIT::jitMethod(Method& method) {
+    using namespace asmjit;
+
+    if (!std::ranges::all_of(method.domain(),
+                             [&](ORef type) { return eq(type, rt_->types.any); })
         ) {
-            // TODO: Generate (non-punting) code for these functions with nontrivial domain.
-            as_.mov(x86::rax, PrimopRes::CALL_BYTECODE);
-            as_.ret();
-        } else {
-            auto const doCheckDomain = as_.new_label();
-            auto const doCheckSpeculation = as_.new_label();
-            auto const argsChecked = as_.new_label();
+        // TODO: Generate (non-punting) code for these functions with nontrivial domain.
+        as_.mov(x86::rax, PrimopRes::CALL_BYTECODE);
+        as_.ret();
+    } else {
+        auto const doCheckDomain = as_.new_label();
+        auto const doCheckSpeculation = as_.new_label();
+        auto const argsChecked = as_.new_label();
 
-            // HRef<Method> const method = rt->regs[calleeReg]->method;
-            size_t const calleeOffset = rt_->regsOffset() + sizeof(ORef) * calleeReg;
-            as_.mov(x86::r10, payloadMask);
-            as_.and_(x86::r10, x86::Mem{X64SYSVJIT::rtReg, int32_t(calleeOffset)});
-            as_.mov(x86::r10, x86::Mem{x86::r10, offsetof(Closure, method)});
+        // HRef<Method> const method = rt->regs[calleeReg]->method;
+        size_t const calleeOffset = rt_->regsOffset() + sizeof(ORef) * calleeReg;
+        as_.mov(x86::r10, payloadMask);
+        as_.and_(x86::r10, x86::Mem{X64SYSVJIT::rtReg, int32_t(calleeOffset)});
+        as_.mov(x86::r10, x86::Mem{x86::r10, offsetof(Closure, method)});
 
-            // RT::DomainChecking const domainChecking = rt->domainChecking;
-            // if (domainChecking == RT::DomainChecking::SKIP) {
-            as_.mov(x86::rax, x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->domainCheckingOffset())});
-            as_.cmp(x86::rax, RT::DomainChecking::SKIP);
-            as_.jne(doCheckDomain);
-            //      domainChecking = RT::DomainChecking::CHECK;
-            as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->domainCheckingOffset())},
-                   RT::DomainChecking::CHECK);
-            //      goto argsChecked;
-            as_.jmp(argsChecked);
-            // }
+        // RT::DomainChecking const domainChecking = rt->domainChecking;
+        // if (domainChecking == RT::DomainChecking::SKIP) {
+        as_.mov(x86::rax, x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->domainCheckingOffset())});
+        as_.cmp(x86::rax, RT::DomainChecking::SKIP);
+        as_.jne(doCheckDomain);
+        //      domainChecking = RT::DomainChecking::CHECK;
+        as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->domainCheckingOffset())},
+                RT::DomainChecking::CHECK);
+        //      goto argsChecked;
+        as_.jmp(argsChecked);
+        // }
 
-            as_.bind(doCheckDomain);
-            // if (domainChecking == RT::DomainChecking::CHECK) {
-            as_.cmp(x86::rax, RT::DomainChecking::CHECK);
-            as_.jne(doCheckSpeculation);
-            //     size_t const argc = state->entryRegc - firstArgReg;
-            as_.mov(x86::rax, x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->entryRegcOffset())});
-            as_.sub(x86::rax, firstArgReg);
-            //     auto const arity = size_t(method->flexCount().val());
-            as_.movabs(x86::r9, payloadMask);
-            as_.and_(x86::r9, x86::r10);
-            as_.and_(x86::r11, x86::Mem{x86::r9, int32_t(flexCountOffset)});
-            //     if (argc != arity) {
-            as_.cmp(x86::rax, x86::r11);
-            as_.je(argsChecked);
-            if (method.hasVarArg.val()) {
-                // TODO: Generate (non-punting) code for this:
-                as_.mov(X64SYSVJIT::retReg, PrimopRes::CALL_BYTECODE);
-                as_.ret();
-            }
-            // Domain check failed. Fall back to interpreter to face consequences (and do redundant
-            // work, but realistically a logic error like this should end the entire process):
-            as_.mov(X64SYSVJIT::retReg, PrimopRes::CALL_BYTECODE);
-            as_.ret();
-            //     }
-            // }
-
-            as_.bind(doCheckSpeculation);
+        as_.bind(doCheckDomain);
+        // if (domainChecking == RT::DomainChecking::CHECK) {
+        as_.cmp(x86::rax, RT::DomainChecking::CHECK);
+        as_.jne(doCheckSpeculation);
+        //     size_t const argc = state->entryRegc - firstArgReg;
+        as_.mov(x86::rax, x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->entryRegcOffset())});
+        as_.sub(x86::rax, firstArgReg);
+        //     auto const arity = size_t(method->flexCount().val());
+        as_.movabs(x86::r9, payloadMask);
+        as_.and_(x86::r9, x86::r10);
+        as_.and_(x86::r11, x86::Mem{x86::r9, int32_t(flexCountOffset)});
+        //     if (argc != arity) {
+        as_.cmp(x86::rax, x86::r11);
+        as_.je(argsChecked);
+        if (method.hasVarArg.val()) {
             // TODO: Generate (non-punting) code for this:
             as_.mov(X64SYSVJIT::retReg, PrimopRes::CALL_BYTECODE);
             as_.ret();
+        }
+        // Domain check failed. Fall back to interpreter to face consequences (and do redundant
+        // work, but realistically a logic error like this should end the entire process):
+        as_.mov(X64SYSVJIT::retReg, PrimopRes::CALL_BYTECODE);
+        as_.ret();
+        //     }
+        // }
 
-            as_.bind(argsChecked);
+        as_.bind(doCheckSpeculation);
+        // TODO: Generate (non-punting) code for this:
+        as_.mov(X64SYSVJIT::retReg, PrimopRes::CALL_BYTECODE);
+        as_.ret();
 
-            if (method.hasVarArg.val()) {
-                // TODO: Generate (non-punting) code for vararg reification:
-                as_.mov(X64SYSVJIT::retReg, PrimopRes::CALL_BYTECODE);
-                as_.ret();
-            }
+        as_.bind(argsChecked);
 
-            // rt->method = method;
-            as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->methodOffset())}, x86::r10);
-            // rt->code = HRef<ByteArray>::fromUnchecked(method->code)->flexData();
-            as_.movabs(x86::r11, payloadMask);
-            as_.and_(x86::r10, x86::r11);
-            as_.and_(x86::r11, x86::Mem{x86::r10, offsetof(Method, code)});
-            as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->codeOffset())}, x86::r11);
-            // rt->consts = HRef<ArrayMut>::fromUnchecked(method->consts)->itemsMut().data();
-            as_.movabs(x86::r11, payloadMask);
-            as_.and_(x86::r11, x86::Mem{x86::r10, offsetof(Method, consts)});
-            size_t const constsObjOffset = rt_->constsOffset();
-            as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(constsObjOffset)}, x86::r11);
-            // OPTIMIZE: `SlotsMut<ORef>::slots_` seems redundant for `RT::consts`:
-            size_t const constsSlotsOffset = constsObjOffset + SlotsMut<ORef>::slotsOffset;
-            as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(constsSlotsOffset)}, x86::r11);
-            // rt->pc = Method::entryPc();
-            // `as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->pcOffset())}, Method::entryPc());` was
-            // storing an incorrect value for some reason :(:
-            as_.mov(x86::r11, Method::entryPc());
-            as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->pcOffset())}, x86::r11);
-            // TODO: JIT actual body code:
-            as_.mov(X64SYSVJIT::retReg, PrimopRes::INTERPRET);
+        if (method.hasVarArg.val()) {
+            // TODO: Generate (non-punting) code for vararg reification:
+            as_.mov(X64SYSVJIT::retReg, PrimopRes::CALL_BYTECODE);
             as_.ret();
         }
 
-        MethodCode* entryCode = reinterpret_cast<MethodCode*>(method.code->itemsMut().data());
-        if (rt_->jit.add(entryCode, &code_) != Error::kOk) { PANIC("JIT miscompilation"); }
+        // rt->method = method;
+        as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->methodOffset())}, x86::r10);
+        // rt->code = HRef<ByteArray>::fromUnchecked(method->code)->flexData();
+        as_.movabs(x86::r11, payloadMask);
+        as_.and_(x86::r10, x86::r11);
+        as_.and_(x86::r11, x86::Mem{x86::r10, offsetof(Method, code)});
+        as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->codeOffset())}, x86::r11);
+        // rt->consts = HRef<ArrayMut>::fromUnchecked(method->consts)->itemsMut().data();
+        as_.movabs(x86::r11, payloadMask);
+        as_.and_(x86::r11, x86::Mem{x86::r10, offsetof(Method, consts)});
+        size_t const constsObjOffset = rt_->constsOffset();
+        as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(constsObjOffset)}, x86::r11);
+        // OPTIMIZE: `SlotsMut<ORef>::slots_` seems redundant for `RT::consts`:
+        size_t const constsSlotsOffset = constsObjOffset + SlotsMut<ORef>::slotsOffset;
+        as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(constsSlotsOffset)}, x86::r11);
+        // rt->pc = Method::entryPc();
+        // `as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->pcOffset())}, Method::entryPc());` was
+        // storing an incorrect value for some reason :(:
+        as_.mov(x86::r11, Method::entryPc());
+        as_.mov(x86::Mem{X64SYSVJIT::rtReg, int32_t(rt_->pcOffset())}, x86::r11);
+
+        naturalize(method.code->items());
     }
-};
+
+    MethodCode* entryCode = reinterpret_cast<MethodCode*>(method.code->itemsMut().data());
+    if (rt_->jit.add(entryCode, &code_) != Error::kOk) { PANIC("JIT miscompilation"); }
+}
 
 void jitCompile(RT& rt, Method& method) {
     X64SYSVJIT{rt}.jitMethod(method);
